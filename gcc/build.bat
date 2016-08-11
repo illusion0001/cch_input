431103eecdb3e4207b261d564b85397fdc926143
shekhjkl <shekh.anton@gmail.com>
2016-08-11 12:44:07 +0300

    fix build

--- gcc/build.bat
+++ gcc/build.bat
@@ -4,28 +4,28 @@
 @set log=build.log
 
 @echo compile sources
-@%bin%\windres.exe %src%\ffmpeg.rc %out%\rc.o 2>%log%
+@windres %src%\ffmpeg.rc %out%\rc.o 2>%log%
 
 @set param=-I%src% -I%include% -D__STDC_CONSTANT_MACROS -Wno-deprecated-declarations -Wno-multichar -O2
 
-@%bin%\g++.exe -c %param% %src%\audiosource2.cpp -o %out%\audiosource2.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\cineform.cpp -o %out%\cineform.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\export.cpp -o %out%\export.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\fflayer.cpp -o %out%\fflayer.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\fflayer_render.cpp -o %out%\fflayer_render.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\fileinfo2.cpp -o %out%\fileinfo2.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\inputfile2.cpp -o %out%\inputfile2.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\main2.cpp -o %out%\main2.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\vfmain.cpp -o %out%\vfmain.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\videosource2.cpp -o %out%\videosource2.o 2>>%log%
+@g++ -c %param% %src%\audiosource2.cpp -o %out%\audiosource2.o 2>>%log%
+@g++ -c %param% %src%\cineform.cpp -o %out%\cineform.o 2>>%log%
+@g++ -c %param% %src%\export.cpp -o %out%\export.o 2>>%log%
+@g++ -c %param% %src%\fflayer.cpp -o %out%\fflayer.o 2>>%log%
+@g++ -c %param% %src%\fflayer_render.cpp -o %out%\fflayer_render.o 2>>%log%
+@g++ -c %param% %src%\fileinfo2.cpp -o %out%\fileinfo2.o 2>>%log%
+@g++ -c %param% %src%\inputfile2.cpp -o %out%\inputfile2.o 2>>%log%
+@g++ -c %param% %src%\main2.cpp -o %out%\main2.o 2>>%log%
+@g++ -c %param% %src%\vfmain.cpp -o %out%\vfmain.o 2>>%log%
+@g++ -c %param% %src%\videosource2.cpp -o %out%\videosource2.o 2>>%log%
 
-@%bin%\g++.exe -c %param% %src%\vd2\vdxframe\videofilter.cpp -o %out%\videofilter.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\vd2\vdxframe\videofilterdialog.cpp -o %out%\videofilterdialog.o 2>>%log%
-@%bin%\g++.exe -c %param% %src%\vd2\vdxframe\videofilterentry.cpp -o %out%\videofilterentry.o 2>>%log%
+@g++ -c %param% %src%\vd2\vdxframe\videofilter.cpp -o %out%\videofilter.o 2>>%log%
+@g++ -c %param% %src%\vd2\vdxframe\videofilterdialog.cpp -o %out%\videofilterdialog.o 2>>%log%
+@g++ -c %param% %src%\vd2\vdxframe\videofilterentry.cpp -o %out%\videofilterentry.o 2>>%log%
 
 @set objlist=%out%\rc.o %out%\audiosource2.o %out%\cineform.o %out%\export.o %out%\fflayer.o %out%\fflayer_render.o %out%\fileinfo2.o %out%\inputfile2.o %out%\main2.o %out%\vfmain.o %out%\videofilter.o %out%\videofilterdialog.o %out%\videofilterentry.o %out%\videosource2.o
 @set fflib=-lavcodec -lavformat -lavutil -lswresample -lswscale -lmp3lame -lavcodec
 @set syslib=-lvfw32 -lcomdlg32 -lz -lopus -lvorbis -lvorbisenc -llzma -liconv -lbz2 -lws2_32 -lgmp -lsecur32 -logg
 
 @echo linking
-@%bin%\g++.exe -O2 -static-libgcc -mdll -L%lib% %objlist% %fflib% %syslib% -o %out%\cch_input.vdf 2>>%log%
+@g++ -O2 -static-libgcc -mdll -L%lib% %objlist% %fflib% %syslib% -o %out%\cch_input.vdf 2>>%log%
--- gcc/build32.bat
+++ gcc/build32.bat
@@ -1,6 +1,6 @@
 @set msys=E:\work\ffmpeg_build\msys64
 @set bin=%msys%\mingw32\bin
-@path "%path%;%bin%;%msys%\usr\bin"
+@path %path%;%bin%;%msys%\usr\bin
 @set lib=%msys%\..\local32\lib
 @set include=%msys%\..\local32\include
 @set out=obj32
